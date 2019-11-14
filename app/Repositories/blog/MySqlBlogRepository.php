e646984e5a73d8b9f8b8b92d27acf00186f3c08b
Grga-Pitic <ikon98@gmail.com>
2019-11-15 01:00:47 +0300

    Implements the repository.

--- app/Repositories/blog/MySqlBlogRepository.php
+++ app/Repositories/blog/MySqlBlogRepository.php
@@ -3,19 +3,47 @@
 
 namespace App\Repositories\blog;
 
+use Illuminate\Support\Facades\DB;
 use App\Repositories\blog\base\IBlogRepository;
+use App\Models\PostModel;
+
+DEFINE('TABLE_NAME', 'post');
 
 class MySqlBlogRepository implements IBlogRepository {
     public function getPostById(int $id){
+        return DB::table(TABLE_NAME)->where('id', $id)->first();
+    }
+    public function getLatestPosts() {
+        $result = DB::table(TABLE_NAME)->orderBy('published_at', 'desc')->take(2)->get();
+
+        $modelsArray = array();
+        foreach ($result as $row){
+            array_push($modelsArray, $this->createPostByRow($row));
+        }
+
+        return $modelsArray;
+    }
+    public function getPostList(array $parameters) {
 
     }
-    function getLatestPosts() {
 
+    public function getCount() {
+        return DB::table(TABLE_NAME)->count();
     }
-    function getPostList(array $parameters) {
 
+    private function createPostByRow($row){
+        $data = array();
+        $data['id'] = $row->id;
+        $data['title'] = $row->title;
+        $data['published_at'] = $row->published_at;
+        $data['short'] = $row->short_content;
+        $data['content'] = $row->content;
+        $data['image'] = $row->image;
+
+        $newModel = new PostModel($data);
+
+        return $newModel;
     }
-    function getCount() {
-        return 1;
-    }
+
+
 }
