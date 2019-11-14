e9fbb000fad8f18ac1ad4b0d6f651882fc4996d4
Grga-Pitic <ikon98@gmail.com>
2019-11-15 00:57:13 +0300

    Implements controller for the Home page.

--- app/Http/Controllers/Pages/HomepageController.php
+++ app/Http/Controllers/Pages/HomepageController.php
@@ -3,10 +3,21 @@
 namespace App\Http\Controllers\Pages;
 
 use Illuminate\Http\Request;
+use PDOException;
 use App\Http\Controllers\Controller;
+use App\Repositories\blog\base\IBlogRepository;
 
 class HomepageController extends Controller {
     public function show(Request $request){
-        return "Homepage";
+        $container = app();
+        $repository = $container->make(IBlogRepository::class);
+
+        try {
+            $latestPostsArray = $repository->getLatestPosts();
+            return view('pages.home.page', ['latestPosts' => $latestPostsArray]);
+        } catch (PDOException $e) {
+            return view('pages.DBError');
+        }
+
     }
 }
