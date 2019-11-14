8621c75c55bb0e817500bdae26ccda6716b99f78
Grga-Pitic <ikon98@gmail.com>
2019-11-15 01:05:52 +0300

    Creates the db error page.

---
+++ resources/views/pages/DBError.blade.php
@@ -0,0 +1,7 @@
+@extends('template')
+
+@section('title', ' - Error')
+
+@section('content')
+    <p>An error occurred while working with the database! We apologize! Try again later.</p>
+@endsection
