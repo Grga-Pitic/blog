83634b856ae48597c083ddec05f514c67a8cbb8b
Grga-Pitic <ikon98@gmail.com>
2019-11-16 01:51:14 +0300

    Implements the post page view.

--- resources/views/pages/post/page.blade.php
+++ resources/views/pages/post/page.blade.php
@@ -7,5 +7,36 @@
 
 
 @section('content')
-
+    <!-- main-content-wrap start -->
+    <div class="main-content-wrap shop-page section-ptb">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-7 order-lg-2 oreder-2 offset-lg-1">
+                    <div class="row">
+                        <div class="col-lg-12 blog-details-area">
+                            <div class="blog-details-image mt-30">
+                                <img src="/assets/images/blog/{{$postModel->getDataElement('image')}}" alt="">
+                            </div>
+                            <div class="our-blog-contnet">
+                                <h5>
+                                    <a href="/post/{{$postModel->getDataElement('id')}}">
+                                        {{$postModel->getDataElement('title')}}
+                                    </a>
+                                </h5>
+                                <div class="post_meta">
+                                    <ul>
+                                        <li>
+                                            <p>{{$postModel->getDataElement('published_at')}}</p>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <p>{{$postModel->getDataElement('content')}}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- main-content-wrap end -->
 @endsection
