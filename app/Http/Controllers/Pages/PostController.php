b2ce42dfb295637ae9bb97a521a3a2a39c5497c0
Grga-Pitic <ikon98@gmail.com>
2019-11-16 01:49:24 +0300

    Implements a controller for a post page.

--- app/Http/Controllers/Pages/PostController.php
+++ app/Http/Controllers/Pages/PostController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers\Pages;
 
+use App\Repositories\blog\base\IBlogRepository;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use PDOException;
 
 class PostController extends Controller {
-    //
+    public function show(Request $request, $id){
+        $container = app();
+        $repository = $container->make(IBlogRepository::class);
+
+        try {
+            $postModel = $repository->getPostByID($id);
+            return view('pages.post.page', ['postModel' => $postModel]);
+        } catch (PDOException $e) {
+            return view('pages.DBError');
+        } catch (\ErrorException $e) {
+            abort(404);
+        }
+        return view('pages.post.page');
+    }
 }
