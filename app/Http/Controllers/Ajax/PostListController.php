7e32a5948560e1ccdfc6be78897e93f8d05f484e
Grga-Pitic <ikon98@gmail.com>
2019-11-15 23:30:18 +0300

    Implements a PostListController.

---
+++ app/Http/Controllers/Ajax/PostListController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Ajax;
+
+use App\Repositories\blog\base\IBlogRepository;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+DEFINE ('DEFAULT_QUANTITY_ON_PAGE', 10);
+
+class PostListController extends Controller {
+    public function show(Request $request, $p = 0, $q = DEFAULT_QUANTITY_ON_PAGE){
+
+        $container = app();
+        $repository =  $container->make(IBlogRepository::class);
+
+        $parametersOfList = [
+            'page' => $p,
+            'quantity' => $q,
+        ];
+
+        if($request->input('old-first') == 'true'){
+            $parametersOfList['oldFirst'] = true;
+        } else {
+            $parametersOfList['oldFirst'] = false;
+        }
+
+        $listOfPosts = $repository->getPostList($parametersOfList);
+        $postsTotal  = $repository->getCount();
+
+        return view('pages.blogs.parts.blogList', [
+            'postList' => $listOfPosts,
+            'currentPage' => $p,
+            'pagesQuantity' => $this->calculatePagesQuantity($q, $postsTotal),
+            'pageSize' => $q
+        ]);
+    }
+
+    private function calculatePagesQuantity(int $pageSize, int $postsTotal){
+        $pageQuantity = intdiv($postsTotal, $pageSize);
+        if(($postsTotal % $pageSize) != 0){
+            $pageQuantity++;
+        }
+
+        return $pageQuantity;
+    }
+}
