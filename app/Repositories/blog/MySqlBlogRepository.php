cbd2994b547c465f601f0f7734a6e7bc9c2759ad
Grga-Pitic <ikon98@gmail.com>
2019-11-15 23:32:13 +0300

    Implements the getPostList method for getting a list of a posts.

--- app/Repositories/blog/MySqlBlogRepository.php
+++ app/Repositories/blog/MySqlBlogRepository.php
@@ -16,19 +16,47 @@ class MySqlBlogRepository implements IBlogRepository {
     public function getLatestPosts() {
         $result = DB::table(TABLE_NAME)->orderBy('published_at', 'desc')->take(2)->get();
 
+        $modelsArray = $this->getArrayByResult($result);
+        return $modelsArray;
+    }
+    public function getPostList(array $parameters) {
+        $query = DB::table(TABLE_NAME);
+
+        $this->acceptSort($query, $parameters);
+        $this->acceptLimits($query, $parameters);
+        $result = $query->get();
+
+        $modelsArray = $this->getArrayByResult($result);
+        return $modelsArray;
+    }
+
+    public function getCount() {
+        return DB::table(TABLE_NAME)->count();
+    }
+
+    private function acceptSort($query, $parameters) {
+        if($parameters['oldFirst']){
+            $query->orderBy('published_at', 'asc');
+            return;
+        }
+
+        $query->orderBy('published_at', 'desc');
+
+    }
+
+    private function acceptLimits($query, array $parameters){
+        $minIndex = $parameters['page'] * $parameters['quantity'];
+        $query->skip($minIndex)->take($parameters['quantity']);
+    }
+
+
+    private function getArrayByResult($result){
         $modelsArray = array();
         foreach ($result as $row){
             array_push($modelsArray, $this->createPostByRow($row));
         }
 
         return $modelsArray;
-    }
-    public function getPostList(array $parameters) {
-
-    }
-
-    public function getCount() {
-        return DB::table(TABLE_NAME)->count();
     }
 
     private function createPostByRow($row){
