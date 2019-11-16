3bb4b79722615b8f9687e5ff73a3c50c013019e8
Grga-Pitic <ikon98@gmail.com>
2019-11-16 16:14:25 +0300

    Adds the page not found view.

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,26 @@
+@extends('template')
+
+
+
+@section('Title', ' - 404')
+
+
+
+@section('content')
+    <!-- main-content-wrap start -->
+    <div class="main-content-wrap section-ptb wishlist-page">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 offset-lg-2 text-center">
+                    <div class="search-error-wrapper">
+                        <h1>404</h1>
+                        <h2>PAGE NOT BE FOUND</h2>
+                        <p class="home-link">Sorry but the page you are looking for does not exist, have been removed, name changed or is temporarity unavailable.</p>
+                        <a href="/" class="home-bacck-button">Back to home page</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- main-content-wrap end -->
+@endsection
